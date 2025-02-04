723762a52ed814299d8bbf07e529f5086cab6ae0
Fahari <mkl.devops@gmail.com>
2025-02-04 22:46:58 +0000

    feat: add conexion with google

--- components/auth/google-button.tsx
+++ components/auth/google-button.tsx
@@ -2,16 +2,17 @@
 
 import { Button } from "@/components/ui/button";
 import { SiGoogle } from "@icons-pack/react-simple-icons";
+import { signIn } from "next-auth/react";
 
 export const GoogleButton = () => {
-	const handleClick = () => {
-		console.log("Google button clicked");
-	};
+  const handleClick = () => {
+    signIn("google", { callbackUrl: "/" });
+  };
 
-	return (
-		<Button variant="outline" className="w-full" onClick={handleClick}>
-			<SiGoogle />
-			Google
-		</Button>
-	);
+  return (
+    <Button variant="outline" className="w-full" onClick={handleClick}>
+      <SiGoogle />
+      Google
+    </Button>
+  );
 };

8e499fbfc66441aa7270c7cdc21c65db901701b2
Fahari <mkl.devops@gmail.com>
2026-04-26 17:58:10 +0300

    feat: move user suggestions from home to explore page

--- app/(home)/(public)/page.tsx
+++ app/(home)/(public)/page.tsx
@@ -1,9 +1,6 @@
 import { redirect } from "next/navigation";
-import { Suspense } from "react";
-import { UserSuggestions } from "@/components/feature/user/user-suggestions";
 import { getHasSeenSplash } from "@/src/actions/cookies.actions";
 import { getPostsWithCursor } from "@/src/query/post.query";
-import { getSuggestedUsers, getUserSessionId } from "@/src/query/user.query";
 import Posts from "./posts";
 
 export default async function Home() {
@@ -11,30 +8,10 @@ export default async function Home() {
 		return redirect("/splash");
 	}
 
-	const [initialData, userId] = await Promise.all([
-		getPostsWithCursor({}),
-		getUserSessionId(),
-	]);
-
-	const suggestedUsers = userId ? await getSuggestedUsers(userId, 3) : [];
+	const initialData = await getPostsWithCursor({});
 
 	return (
 		<div>
-			{suggestedUsers.length > 0 && (
-				<Suspense fallback={null}>
-					<div className="px-4 pt-4">
-						<UserSuggestions
-							users={suggestedUsers.map((u) => ({
-								id: u.id,
-								name: u.name ?? "",
-								image: u.image,
-								userType: u.userType ?? "",
-								position: u.position,
-							}))}
-						/>
-					</div>
-				</Suspense>
-			)}
 			<Posts initialData={initialData} />
 		</div>
 	);
